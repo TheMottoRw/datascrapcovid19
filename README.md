844ff77e9249ab6c56413d9462c98ef01dd70d2d
TheMottoRw <hasua.mr@gmail.com>
2020-09-02 17:52:47 +0200

    data scrapping init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# datascrapcovid19
+# Scrap data of COVID19 
+
+This project is intended to get data of COVID19 pandemic,organize for future application development related to COVID19
+
+## Getting started
+
+- You should have python installed version 3.x
+- Use <code>pip</code> to install required library from requirements.txt
+- Run <code>pip3 install -r requirements.txt</code> 
+- Modify user,domain and path of your remote server to upload in scrapped data <code>os.system("scp "+file+" user@domain:public_html/path/"+file)</code>
+
+## Contribution
+
+- Feel free to contribute to this project I will be merging your work
---
+++ scrap.py
@@ -0,0 +1,80 @@
+import requests,csv,os,subprocess
+from datetime import datetime,timedelta
+from bs4 import BeautifulSoup
+
+
+url = "https://www.worldometers.info/coronavirus/"
+r = requests.get(url)
+#print (r.content)
+soup = BeautifulSoup(r.content,'html5lib')
+#print(soup.prettify())
+
+table = soup.find("table",attrs = {'id':'main_table_countries_today'})
+tableYesterday = soup.find("table",attrs = {'id':'main_table_countries_yesterday'})
+header = {}
+th = table.findAll("thead")[0].findAll("tr")[0].findAll("th")
+tbody = table.findAll("tbody")
+
+tbodyYesterday = tableYesterday.findAll("tbody")
+
+#get table header
+i=0
+while i < len(th):
+    header[th[i].text] = th[i].text
+    i=i+1
+
+
+def getFileName(day):
+    if(day == 'today'):
+        date_time_obj = str(datetime.today())[:10]
+    else:#yesterday
+        date_time_obj = str(datetime.today() - timedelta(days = 1))[:10]
+    dateArr = date_time_obj.split("-")
+    newDate = dateArr[1]+"-"+dateArr[2]+"-"+dateArr[0]
+
+    return newDate
+
+def getBodyContent(arr):
+    contents = []
+    for tbody in arr:
+        for tr in tbody.findAll("tr"):
+            td = tr.findAll("td")
+            body = {}
+            i=0
+            while i < len(td):
+                body[th[i].text] = td[i].text.replace("\n","").replace("\n","").replace(" ","")
+                i=i+1
+            
+            contents.append(body)
+    return contents
+fileContent = []
+#csv file to save content in
+
+file = getFileName('today')+'.csv'
+fileYesterday = getFileName('yesterday')+'.csv'
+
+
+covidData = getBodyContent(tbody)
+covidDataYesterday = getBodyContent(tbodyYesterday)
+#covidData.insert(0, header)
+
+with open(file,'w') as f:
+    w = csv.DictWriter(f,header)
+    w.writeheader()
+    for row in covidData:
+        w.writerow(row)
+
+
+with open(fileYesterday,'w') as f:
+    w = csv.DictWriter(f,header)
+    w.writeheader()
+    for row in covidDataYesterday:
+        w.writerow(row)
+
+#upload file to server
+#upload file on server
+# os.system("cp "+file+" ~/Documents")
+# os.system("cp "+fileYesterday+" ~/Documents")
+os.system("scp "+file+" user@domain:public_html/path/"+file)
+
+#sts = os.waitpid(p.pid, 0)
